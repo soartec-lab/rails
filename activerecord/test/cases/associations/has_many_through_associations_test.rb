b2146046e0e1a4a8a1ee9126920d133fa0ba823d
soartec-lab <info@soartec-lab.work>
2019-11-21 17:45:08 +0900

    assert_difference is Person.count to -1

--- activerecord/test/cases/associations/has_many_through_associations_test.rb
+++ activerecord/test/cases/associations/has_many_through_associations_test.rb
@@ -429,7 +429,7 @@ class HasManyThroughAssociationsTest < ActiveRecord::TestCase
   end
 
   def test_destroy_all!
-    assert_no_difference "Person.count" do
+    assert_difference "Person.count", -1 do
       assert_no_difference "Reader.count" do
         posts(:welcome).people.destroy_all!
       end
